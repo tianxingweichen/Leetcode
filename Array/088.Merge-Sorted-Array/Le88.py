1680363d56f712c6cd23486fe1d13f34f552e913
Dongwenpu <m17761267293.163.com>
2017-04-02 14:58:52 +0800

    finish

--- Array/088.Merge-Sorted-Array/Le88.py
+++ Array/088.Merge-Sorted-Array/Le88.py
@@ -1,2 +1,9 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
+        for i in range(n):
+            nums1.append(nums2[i])
+        while nums1.count(0) > 0:
+            nums1.remove(0)
+        nums1.sort()
+        return nums1
+
