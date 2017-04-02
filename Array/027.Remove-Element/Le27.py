ec6e09d189a2e83fcdeafa75de6ba03f52d1ad96
Dongwenpu <m17761267293.163.com>
2017-04-02 14:34:28 +0800

    program

--- Array/027.Remove-Element/Le27.py
+++ Array/027.Remove-Element/Le27.py
@@ -1,3 +1,6 @@
 class Solution(object):
     def removeElement(self, nums, val):
-
+        num = nums.count(val)
+        for i in range(num):
+            nums.remove(val)
+        return len(num)
